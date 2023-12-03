2f5b5f7ede4b5819fbe029dd4aa3334f2ef13d4e
Aras <152716877+arasss1@users.noreply.github.com>
2023-12-03 10:24:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+kelime_sozlugu = {
+            "ROFL" : "Bir şakaya karşılık cevap",
+            "AGGRO" : "Agresifleşmek/sinirlenme",
+            "CREEPY" : "Korkunç",
+            "SHEESH" : "Onaylamamak",
+            "LOL" : "Komik bir şeye verilen cevap"
+    
+            }
+            
+            
+print(kelime_sozlugu["LOL"])
